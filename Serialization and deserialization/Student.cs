0d8536544682c52477522ee5199df6a8c73c0fb8
Baben_fu97@student.itstep.org <Baben_fu97@student.itstep.org>
2023-12-19 21:50:08 +0200

    #Task 4 Добавление методов в класс Student.
    Метод Print - для вывода свойств обьекта класса на экран.
    Метод Add - для ввода даных пользователем в свойств обьекта класса.

--- Serialization and deserialization/Student.cs
+++ Serialization and deserialization/Student.cs
@@ -24,5 +24,18 @@ namespace Serialization_and_deserialization
             Average = average;
             Number_Of_Group = number_of_group;
         }
+        public void Print()
+        {
+            base.Print();
+            Console.Write($"\t\t{Average}\t\t{Number_Of_Group}\n");
+        }
+        public void Add()
+        {
+            base.Add();
+            Console.WriteLine("Введите средний балл студента:");
+            Average = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите номер группы студента:");
+            Number_Of_Group = int.Parse(Console.ReadLine());
+        }
     }
 }
