f5602d2726317819909ebf56a046fec8ab767a45
Baben_fu97@student.itstep.org <Baben_fu97@student.itstep.org>
2023-12-19 21:48:33 +0200

    #Task 3 Создание класса Student.
    Производній класс Student описываюший сушность студент для дальнейшего хранения в колекции класса AcadenyGrup.

---
+++ Serialization and deserialization/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization_and_deserialization
+{
+    [Serializable]
+    [DataContract]
+    public class Student : Person
+    {
+        [DataMember]
+        public double Average { get; set; }
+        [DataMember]
+        public int Number_Of_Group { get; set; }
+        public Student() : this(string.Empty, string.Empty, 0, string.Empty, 0, 0)
+        {
+
+        }
+        public Student(string name, string surname, int age, string phone, double average, int number_of_group)
+            : base(name, surname, age, phone)
+        {
+            Average = average;
+            Number_Of_Group = number_of_group;
+        }
+    }
+}
