dfad024df19c16065e3b6dc7a322433f3d1c30ae
Baben_fu97@student.itstep.org <Baben_fu97@student.itstep.org>
2023-12-19 20:39:22 +0200

    #Task 1 Создание абстрактного класса Person.
    Класс от которого будет наследоватся класс Student.
    В классе реализованы конструктор без параметров и конструктор с параметрами. Класс реализован с возможностью сериализации.

---
+++ Serialization and deserialization/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization_and_deserialization
+{
+
+    [Serializable]
+    [KnownType(typeof(Student))]
+    [XmlInclude(typeof(Student))]
+    [DataContract]
+    public abstract class Person
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Surname { get; set; }
+        [DataMember]
+        public int Age { get; set; }
+        [DataMember]
+        public string Phone { get; set; }
+        public Person()
+            : this(string.Empty, string.Empty, 0, string.Empty)
+        {
+
+        }
+        public Person(string name, string surname, int age, string phone)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Phone = phone;
+        }
+
+        
+    }
+}
